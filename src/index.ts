330229b843ac70e73bbf5ea5557b730e6144b8ec
yourfriendoss <friend@yourfriend.lv>
2021-11-21 17:54:08 +0300

    fakemute (not done yet)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Client, Intents, Collection } from 'discord.js';
-import { readdirSync, readFileSync } from 'fs';
+import { Client, Intents, Collection, GuildMember } from 'discord.js';
+import { readdirSync } from 'fs';
 import Command from './types/Command.js';
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const commands: Collection<String, Command> = new Collection();
 
@@ -27,5 +27,25 @@ client.on('interactionCreate', async interaction => {
 
 	command.emit("interaction", interaction);
 });
+
+client.on("messageCreate", async message => {
+	/*
+	DiscordAPIError: Unknown User
+    at RequestHandler.execute (C:\Users\pirdi\Desktop\Codes\neco-arc\node_modules\discord.js\src\rest\RequestHandler.js:349:13)
+    at processTicksAndRejections (node:internal/process/task_queues:96:5)
+    at async RequestHandler.push (C:\Users\pirdi\Desktop\Codes\neco-arc\node_modules\discord.js\src\rest\RequestHandler.js:50:14)
+    at async GuildMemberManager._fetchSingle (C:\Users\pirdi\Desktop\Codes\neco-arc\node_modules\discord.js\src\managers\GuildMemberManager.js:388:18)
+    at async Client.<anonymous> (file:///C:/Users/pirdi/Desktop/Codes/neco-arc/src/index.js:22:18) {
+  method: 'get',
+  path: '/guilds/899763349162700850/members/910863136029028372',
+  code: 10013,
+  httpStatus: 404,
+  requestData: { json: undefined, files: [] }*/
+  
+	const user: GuildMember = await message.guild.members.fetch(message.author);
+	if(user.roles.cache.find(e => e.name == "neco-arc-fakemute")) {
+		await message.delete();
+	}
+})
 
 client.login(process.env.TOKEN);
