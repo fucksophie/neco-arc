aa9627ca4aa449442f22c10c4b9141820897002a
yourfriendoss <friend@yourfriend.lv>
2021-11-28 16:37:34 +0300

    Add invite command

---
+++ src/commands/invite.js
@@ -0,0 +1,16 @@
+import  { SlashCommandBuilder } from '@discordjs/builders';
+
+import Command from '../utils/Command.js';
+import EmbedEngine from '../utils/EmbedEngine.js';
+
+const ping = new Command(
+    new SlashCommandBuilder()
+        .setName('invite')
+        .setDescription('Invite the bot!')
+);
+
+ping.on("interaction", async interaction => {
+    await interaction.reply({ embeds: [ EmbedEngine.success(`;) Invite the bot!`).setURL(`https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=0&scope=applications.commands%20bot`) ] });
+});
+
+export default ping;
