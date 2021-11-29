e60bcb1f8099b3595a9907f33fe7d67170a20cb0
yourfriendoss <friend@yourfriend.lv>
2021-11-29 14:15:35 +0300

    add timestamps to embeds

--- src/utils/EmbedEngine.js
+++ src/utils/EmbedEngine.js
@@ -5,18 +5,21 @@ export default class EmbedEngine {
 	static error(message)  {
 		return new MessageEmbed()
 			.setColor("DARK_RED")
+			.setTimestamp()
 			.setTitle("❌ " + message);
 	}
 	
 	static success(message)  {
 		return new MessageEmbed()
 			.setColor("GREEN")
+			.setTimestamp()
 			.setTitle("💚 " + message);
 	}
 
 	static loading(message)  {
 		return new MessageEmbed()
 			.setColor("GREYPLE")
+			.setTimestamp()
 			.setTitle("⚙️ " + message);
 	}	
 	
