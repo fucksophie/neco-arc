26301030cb95ed17acd2da72f7d69c5da2c58285
friend <friend@yourfriend.lv>
2021-12-19 00:18:48 +0200

    Tweet error checking

--- src/commands/tweet.js
+++ src/commands/tweet.js
@@ -6,6 +6,7 @@ import {readFileSync} from "fs";
 const config = JSON.parse(readFileSync("./config.json").toString());
 import { TwitterApi } from 'twitter-api-v2';
 import { MessageEmbed } from 'discord.js';
+import EmbedEngine from '../utils/EmbedEngine.js';
 
 const tweet = new Command(
     new SlashCommandBuilder()
@@ -22,18 +23,23 @@ tweet.on("interaction", async interaction => {
 
 	const client = new TwitterApi(config.keys.twitter);
 	
-	const tweetRequest = await client.v1.tweet(`${interaction.options.getString('tweet', true)}\nMessage sent by ${interaction.user.username}#${interaction.user.discriminator}!`);
+	try {
+		const tweetRequest = await client.v1.tweet(`${interaction.options.getString('tweet', true)}\nMessage sent by ${interaction.user.username}#${interaction.user.discriminator}!`);
+		
+		await interaction.editReply({ embeds: [
+			new MessageEmbed()
+				.setAuthor(`${tweetRequest.user.name} (@${tweetRequest.user.screen_name})`, tweetRequest.user.profile_image_url_https)
+				.setURL(`https://twitter.com/${tweetRequest.user.screen_name}/status/${tweetRequest.id_str}`)
+				.setDescription(tweetRequest.full_text)
+				.setTitle("Tweet sucesfully posted!")
+				.setFooter("Twitter & neco-arc", "https://abs.twimg.com/icons/apple-touch-icon-192x192.png")
+				.setColor("#1DA1F2")
+				.setTimestamp()
+		]});
+	} catch (o_o){
+		await interaction.editReply({ embeds: [ EmbedEngine.error("Couldn't post the tweet.")]});
+	}
 
-	await interaction.editReply({ embeds: [
-		new MessageEmbed()
-			.setAuthor(`${tweetRequest.user.name} (@${tweetRequest.user.screen_name})`, tweetRequest.user.profile_image_url_https)
-			.setURL(`https://twitter.com/${tweetRequest.user.screen_name}/status/${tweetRequest.id_str}`)
-			.setDescription(tweetRequest.full_text)
-			.setTitle("Tweet sucesfully posted!")
-			.setFooter("Twitter & neco-arc", "https://abs.twimg.com/icons/apple-touch-icon-192x192.png")
-			.setColor("#1DA1F2")
-			.setTimestamp()
-	]});
 });
 
 export default tweet;
