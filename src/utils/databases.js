2a182842a4af5cebb5af6aa17ba5ffd01e2f1444
friend <friend@yourfriend.lv>
2021-12-19 01:24:34 +0200

    sqlite ->> mongo

--- src/utils/databases.js
+++ src/utils/databases.js
@@ -1,12 +1,21 @@
 import Josh from '@joshdb/core';
-import provider from '@joshdb/sqlite';
+import provider from '@joshdb/mongo';
+import config from './Config.js';
 
 export const markov = new Josh({
   name: 'MarkovDB',
   provider,
+  providerOptions: {
+    collection: "markov",
+    url: config.keys.mongo
+  }
 });
 
 export const lastfm = new Josh({
   name: 'lastfm',
   provider,
+  providerOptions: {
+    collection: "lastfm",
+    url: config.keys.mongo
+  }
 });
