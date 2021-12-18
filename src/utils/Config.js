f98e24d663cd72c4cce6e8de199e71ffe191067d
friend <friend@yourfriend.lv>
2021-12-19 01:30:58 +0200

    Use mongo_url env variable

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -6,6 +6,10 @@ if(existsSync("./config.json")) {
 	config = JSON.parse(readFileSync("./config.json").toString())
 } else {
 	config = JSON.parse(process.env["JSON_CONFIG"])
+
+	if(process.env["MONGO_URL"]) {
+		config.keys.mongo = process.env["MONGO_URL"];
+	}
 }
 
 export default config
